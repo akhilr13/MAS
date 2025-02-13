3f7345104488bbf9d6ae8fc40d663ffc27452bab
Akhil R <80683465+akhilr13@users.noreply.github.com>
2025-02-13 17:12:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -89,5 +89,5 @@ def simulate():
     return render_template('simulate_form.html')
 
 if __name__ == '__main__':
-        import uvicorn
-        uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get('PORT', 8000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
